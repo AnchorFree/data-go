f4692943eb689f6aef4f08a5a508e5cb39e64a08
Yaroslav Molochko <y.molochko@anchorfree.com>
2018-10-15 12:49:43 +0300

    Enable CIDR based matching

--- pkg/geo/geo_test.go
+++ pkg/geo/geo_test.go
@@ -1,8 +1,9 @@
 package geo
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGeo(t *testing.T) {
@@ -10,13 +11,15 @@ func TestGeo(t *testing.T) {
 		107.181.165.223 af;
 		184.170.253.178 af;
 		107.152.103.226 af;
+		107.152.104.0/24 af;
 		108.62.39.90 af;
 		199.115.116.166 af;
 		198.8.84.226 af;
 		`)
 	g := NewGeo()
 	g.LoadFromBytes(data)
-	assert.Equalf(t, 6, g.Len(), "Amount of IPs does not match")
+	assert.Equalf(t, 7, g.Len(), "Amount of IPs does not match")
 	assert.Equalf(t, "af", g.Get("184.170.253.178"), "Did not find af record")
+	assert.Equalf(t, "af", g.Get("107.152.104.4"), "Did not find af record")
 	assert.Equalf(t, DefaultValue, g.Get("8.8.8.8"), "Should be default value")
 }
