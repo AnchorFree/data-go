888cb9d7d798e1f6bcc6d3f8576fd019776b3552
eugeneb <ebedarev@anchorfree.com>
2019-06-13 19:40:38 -0700

    add file_watcher

---
+++ pkg/file_watcher/watcher.go
@@ -0,0 +1,58 @@
+package file_watcher
+
+import (
+	"github.com/fsnotify/fsnotify"
+	"path/filepath"
+	"time"
+)
+
+var DefaultTimeoutAfterLastEvent = 5 * time.Second
+
+type T struct {
+	file                  string
+	watcher               *fsnotify.Watcher
+	cb                    func(file string)
+	TimeoutAfterLastEvent time.Duration
+}
+
+func New(file string, callback func(file string)) (*T, error) {
+
+	var err error
+	w := &T{
+		file:                  file,
+		cb:                    callback,
+		TimeoutAfterLastEvent: DefaultTimeoutAfterLastEvent,
+	}
+	w.watcher, err = fsnotify.NewWatcher()
+	absPath, err := filepath.Abs(file)
+	if err != nil {
+		return nil, err
+	}
+	w.watcher.Add(filepath.Dir(absPath))
+
+	if err != nil {
+		return nil, err
+	}
+	go func(w *T) {
+		var timer *time.Timer
+		for {
+			select {
+			case event := <-w.watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
+					if filepath.Base(event.Name) == w.file || event.Name == w.file {
+						if timer != nil {
+							timer.Stop()
+						}
+						timer = time.AfterFunc(w.TimeoutAfterLastEvent, func() { w.cb(event.Name) })
+					}
+				}
+				/*
+					case err, ok := <-w.watcher.Errors:
+						logger.Get().Debugf("error: %v", err)
+				*/
+			}
+		}
+	}(w)
+
+	return w, nil
+}
